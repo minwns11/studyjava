6ed8d947b3d09dc92b3ddebff9d68aea59be49cb
minwns11 <yym5066666666@gmail.com>
2024-05-31 21:05:49 +0900

    Initial commit web
    Initial commit web

--- src/test/java/com/sbmavenweb/study001/exam001Test.java
+++ src/test/java/com/sbmavenweb/study001/exam001Test.java
@@ -1,37 +1,27 @@
 package com.sbmavenweb.study001;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class exam001Test {
     @Test
     public void subtract001() throws Exception {
-        // given        테스트 하기 위한 준비 동작
-        int num1 = 2;
-        int num2 = 3;
-        int expected = -1;
-        
-        // when         테스트 실시
-        try {
-            int actual = subTest001(num1, num2);
-        } catch (Exception ex) {
-
-        }
-        // then         실제값과 기대값을 비교해서 테스트한다.
-        assertThat(-1).isEqualTo(subTest001(2, 3)); //값:-1 값이 -1일 시 넘김
-        assertThat(98).isEqualTo(subTest001(100, 2));
-        assertThat(7).isEqualTo(subTest001(10, 3));
-        assertThat(10).isNotEqualTo(subTest001(100, 70)); //값:30 값이 10일 시 잡음
-        assertThat(0).isLessThan(subTest001(100, 70)); // 값:30 0이하 시 잡음
-        assertThat(301).isGreaterThan(subTest001(1000, 700)); // 값:300 301이상 시 잡음
-        //assertThat(actual).isEqualTo(expected).describedAs("실제값과 다름");
+        // Exception 에외가 발생해야지만 정상 동작으로 인식한다.
+        assertThatThrownBy(() -> subTest001(70000, 7))
+                .isInstanceOf(Exception.class);
+        //RuntimeException 예외가 발생해야지만 정상 동작으로 인식한다.
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            subTest001(2330, 50002);
+        });
+        System.out.println(exception.toString());
     }
     int subTest001(int n1, int n2) throws Exception {
         if (n1 < -50000 || n1 > 50000) {
             throw new Exception("num1 값은 -50000보다 작거나, num1 값은 50000보다 큽니다..");
         }
         if (n2 < -50000 || n2 > 50000) {
-            throw new Exception("num2 값은 -50000보다 작거나, num2 값은 50000보다 큽니다..");
+            throw new RuntimeException("num2 값은 -50000보다 작거나, num2 값은 50000보다 큽니다..");
         }
         return n1 - n2;
     }
