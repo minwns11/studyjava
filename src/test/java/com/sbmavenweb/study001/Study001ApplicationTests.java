94c7be72e3f9a46ae2f925f89f2b35f797592310
minwns11 <yym5066666666@gmail.com>
2024-05-31 19:01:23 +0900

    w

---
+++ src/test/java/com/sbmavenweb/study001/Study001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbmavenweb.study001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
